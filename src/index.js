5db4ddf6b7339e0284621633e941e4fdadfae37d
Moise-code <moisewowe@gmail.com>
2022-11-02 11:20:33 +0200

    add the javascript codes for the to do list structure

--- src/index.js
+++ src/index.js
@@ -1,14 +1,44 @@
-import _ from 'lodash';
 import './style.css';
 
- function component() {
-   const element = document.createElement('div');
+const todoDiv = document.querySelector('.todo_lists');
 
-  // Lodash, now imported by this script
-   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-   element.classList.add('hello');
+const todoTasks = [
+  {
+    description: 'Learn and study About Webpack',
+    completed: false,
+    index: 0,
+  },
+  {
+    description: 'Try our Hackerank webpack challenge',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'Take a nap during the launch break',
+    completed: false,
+    index: 2,
+  },
+  {
+    description: 'Attend stand-up meeting',
+    completed: false,
+    index: 3,
+  },
+];
 
-   return element;
- }
+const getTodoTasks = ({ description, index }) => {
+  const div = document.createElement('div');
+  div.className = 'todo-item';
+  div.innerHTML = `
+    <div class="todo_detail">
+    <input type="checkbox" id="" name="" value=""> 
+    <h3 class="item">${description}</h3> <i></i>
+    </div>
+    <i class="fa-solid fa-trash-can" id"${index}"></i>
+    `;
 
- document.body.appendChild(component());
+  return div;
+};
+
+todoTasks.forEach((item) => {
+  todoDiv.append(getTodoTasks(item));
+});
